9b7745819655da4282504bb857171485b1d2b744
Esmer <bd7tqjs7c@code.edu.az>
2024-05-05 21:29:46 +0400

    FirstWebFurniture

---
+++ WebApplication2/Controllers/FurnitureController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class FurnitureController : Controller
+    {
+        public IActionResult Index()
+        {
+            Furniture furniture = new Furniture()
+            {
+                Id = 1,
+                Name = "Chair01",
+                Price = 100
+            };
+            Furniture furniture1 = new Furniture()
+            {
+                Id = 2,
+                Name="Chair02",
+                Price = 100
+
+            };
+            Furniture furniture2 = new Furniture()
+            {
+              Id= 3,
+              Name="Table",
+              Price = 100
+            };
+            List<Furniture> furnitures = new List<Furniture>();
+            furnitures.Add(furniture);
+            ViewBag.Data = furniture;
+
+            return View();
+        }
+    }
+}
